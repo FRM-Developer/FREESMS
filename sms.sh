7b94b5e6592af015ec08044359e273a4621a1f34
Inti Tutorial <53834725+IntiTutorial@users.noreply.github.com>
2020-02-02 11:43:14 +0700

    Update sms.sh

--- sms.sh
+++ sms.sh
@@ -1,5 +1,5 @@
 clear
 echo 
-echo "HARAP TUNGGU SEBENTAR"
-echo "TOOLS INI SEDANG DI PERBAIKI OLEH Inti Tutorial"
-echo "SILAHKAN kembali lagi jam 13.00 WIB"
+echo "HARAP TUNGGU SEBENTAR" | lolcat
+echo "TOOLS INI SEDANG DI PERBAIKI OLEH Inti Tutorial" | lolcat
+echo "SILAHKAN kembali lagi jam 13.00 WIB" | lolcat
